056763d082beead7f5da9da70c57bc0056a58a8f
Patrick O'Hora <patrick55529@gmail.com>
2019-03-05 18:02:52 +0000

    Receive Input

--- src/com/ohora/Main.java
+++ src/com/ohora/Main.java
@@ -1,13 +1,39 @@
 package com.ohora;
 
+import jdk.nashorn.internal.scripts.JO;
+
+import javax.swing.JOptionPane;
 public class Main {
 
     public static void main(String[] args) {
+        // TODO update size so that puzzle can be any size
 
-        // takeInputStartAndValidate
+        int[] startState = new int[9];
+        int[] endState = new int[9];
+
+        startState = receiveInput("start");
+        endState = receiveInput("end");
+
         // take input end and validate
-        // Evaluate full state space here
-        // calculate and display possible movements
-        // calculate h(far away from finish) function of each movement
+        // get possible movements
+        // calculate h(distances of tiles from their pos) function of each movement
+        // display possible
     }
+
+    private static int[] receiveInput(String state){
+        String input = JOptionPane.showInputDialog("Please enter the " + state + " state of the puzzle");
+        //if(validInput)
+        String[] inputArray = input.trim().split(" ");
+        int[] intArray =new int[9];
+        for(int i = 0;i<9;i++){
+            intArray[i] = Integer.parseInt(inputArray[i]);
+        }
+        return intArray;
+    }
+
+//    private static boolean validateInput(String input){
+//
+//    }
+
+
 }
