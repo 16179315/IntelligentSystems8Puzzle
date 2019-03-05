f6796ba1f9fe401953fce1a4201b6b434065c5df
Patrick O'Hora <patrick55529@gmail.com>
2019-03-05 20:11:33 +0000

    Input and Validated UGLY

--- src/com/ohora/Main.java
+++ src/com/ohora/Main.java
@@ -3,37 +3,80 @@ package com.ohora;
 import jdk.nashorn.internal.scripts.JO;
 
 import javax.swing.JOptionPane;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
+
+    public static final int SIZE_OF_PUZZLE = 9;
 
     public static void main(String[] args) {
         // TODO update size so that puzzle can be any size
 
-        int[] startState = new int[9];
-        int[] endState = new int[9];
+        int[] startState = new int[SIZE_OF_PUZZLE];
+        int[] endState = new int[SIZE_OF_PUZZLE];
 
         startState = receiveInput("start");
         endState = receiveInput("end");
 
-        // take input end and validate
+
         // get possible movements
         // calculate h(distances of tiles from their pos) function of each movement
         // display possible
     }
 
-    private static int[] receiveInput(String state){
-        String input = JOptionPane.showInputDialog("Please enter the " + state + " state of the puzzle");
-        //if(validInput)
-        String[] inputArray = input.trim().split(" ");
-        int[] intArray =new int[9];
-        for(int i = 0;i<9;i++){
-            intArray[i] = Integer.parseInt(inputArray[i]);
+    /**
+     * Receives Input and returns valid configuration
+     * @param requestedState
+     * @return
+     */
+    private static int[] receiveInput(String requestedState) {
+        String input = "";
+        int[] intArray;
+        boolean invalidInput = true;
+
+        while (invalidInput) {
+            input = JOptionPane.showInputDialog("Please enter the " + requestedState + " state of the puzzle");
+            if(!validateInput(input)) {
+                JOptionPane.showMessageDialog(null, "Invalid input", "Name", JOptionPane.ERROR_MESSAGE);
+            }
+            invalidInput = !validateInput(input);
         }
+        String[] inputArray = input.trim().split(" ");
+        intArray = Arrays.stream(inputArray).mapToInt(Integer::parseInt).toArray();
+
         return intArray;
     }
 
-//    private static boolean validateInput(String input){
-//
-//    }
+    /**
+     * Checks if all numbers are unique and in the desired range
+     * @param inputString
+     * @return
+     */
+    private static boolean validateInput(String inputString) {
+        String[] inputStringArray = inputString.trim().split(" ");
+        int[] numbers;
+        try {
+            numbers = Arrays.stream(inputStringArray).mapToInt(Integer::parseInt).toArray();
+        } catch(NumberFormatException e){
+            return false;
+        }
+        Set<Integer> uniqueNumbers = new HashSet<Integer>();
+        for(int number : numbers) {
+            if (uniqueNumbers.contains(number) || (number < 0) || number > SIZE_OF_PUZZLE - 1) {
+                return false;
+            }
+            uniqueNumbers.add(number);
+        }
+        if(uniqueNumbers.size() != SIZE_OF_PUZZLE){
+            return false;
+        }
+        System.out.print("valid");
+        return true;
+    }
 
 
 }
