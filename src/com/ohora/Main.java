b0b0fb0c024f93840068e05d96786ce081ad1751
Patrick O'Hora <patrick55529@gmail.com>
2019-03-05 16:30:09 +0000

    Pseudo

---
+++ src/com/ohora/Main.java
@@ -0,0 +1,13 @@
+package com.ohora;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // takeInputStartAndValidate
+        // take input end and validate
+        // Evaluate full state space here
+        // calculate and display possible movements
+        // calculate h(far away from finish) function of each movement
+    }
+}
