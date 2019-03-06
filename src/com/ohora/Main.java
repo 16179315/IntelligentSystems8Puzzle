7449d08e555321c232e55c3eada1c9fe43b3b1ea
Patrick O'Hora <patrick55529@gmail.com>
2019-03-06 00:29:30 +0000

    Added possible move check for middle

--- src/com/ohora/Main.java
+++ src/com/ohora/Main.java
@@ -11,10 +11,12 @@ import java.util.regex.Pattern;
 
 public class Main {
 
-    public static final int SIZE_OF_PUZZLE = 9;
+    // TODO update size so that puzzle can be any size
+    public static final int NUMBER_OF_ROWS = 4;
+    public static final int SIZE_OF_PUZZLE = NUMBER_OF_ROWS*NUMBER_OF_ROWS;
+    public static int[] squaresAtMiddleOfPuzzle = findAllSquaresAtMiddleOfPuzzle();
 
     public static void main(String[] args) {
-        // TODO update size so that puzzle can be any size
 
         int[] startState = new int[SIZE_OF_PUZZLE];
         int[] endState = new int[SIZE_OF_PUZZLE];
@@ -22,10 +24,53 @@ public class Main {
         startState = receiveInput("start");
         endState = receiveInput("end");
 
-
+        int[] possibleSwaps = findPossibleSwaps(startState);
         // get possible movements
         // calculate h(distances of tiles from their pos) function of each movement
         // display possible
+    }
+
+    private static int[] findAllSquaresAtMiddleOfPuzzle() {
+        int[] puzzle = new int[SIZE_OF_PUZZLE];
+        //TODO find out what to subtract by relatively
+        double numberOfSquaresInMiddle = SIZE_OF_PUZZLE - 4*(NUMBER_OF_ROWS-1);
+
+        int row = 0;
+        int col = 0;
+        int middleSqaureIndex = 0;
+        for(int i =0;i<=SIZE_OF_PUZZLE;i+=NUMBER_OF_ROWS){
+            row++;
+            for(int j = i; j<= NUMBER_OF_ROWS-1+i && j != SIZE_OF_PUZZLE; j++){
+                col++;
+                System.out.print("i:" + String.valueOf(i) + " j:" + String.valueOf(j)+  " " +String.valueOf(puzzle[j]) + " ");
+                if(!(row == 1 || row == NUMBER_OF_ROWS || col == 1 || col ==NUMBER_OF_ROWS)){
+                    squaresAtMiddleOfPuzzle[middleSqaureIndex] = j;
+                    middleSqaureIndex++;
+                }
+            }
+            col=0;
+            System.out.print("\n");
+        }
+        return squaresAtMiddleOfPuzzle;
+    }
+
+
+    private static int[] findPossibleSwaps(int[] state) {
+        int indexOfZero = Arrays.asList(state).indexOf(0);
+        int[] possibleSwaps = new int[4];
+        for(int i = 0;i<4;i++) {
+            if (zeroIsAtMiddlePiece(indexOfZero)) {
+                possibleSwaps[i] = indexOfZero - 1;
+                possibleSwaps[i] = indexOfZero + 1;
+                possibleSwaps[i] = indexOfZero + NUMBER_OF_ROWS;
+                possibleSwaps[i] = indexOfZero - NUMBER_OF_ROWS;
+            }
+        }
+        return possibleSwaps;
+    }
+
+    private static boolean zeroIsAtMiddlePiece(int indexOfZero) {
+        return Arrays.asList(squaresAtMiddleOfPuzzle).contains(indexOfZero);
     }
 
     /**
@@ -37,6 +82,7 @@ public class Main {
         String input = "";
         int[] intArray;
         boolean invalidInput = true;
+
 
         while (invalidInput) {
             input = JOptionPane.showInputDialog("Please enter the " + requestedState + " state of the puzzle");
