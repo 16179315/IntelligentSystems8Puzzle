c90a8f3cad86ea8134d5c35e9c4606d726553ab7
16179315 <patrick55529@gmail.com>
2019-03-12 20:08:06 +0000

    H value calculated correctly I think..

--- src/com/ohora/Main.java
+++ src/com/ohora/Main.java
@@ -1,10 +1,7 @@
 package com.ohora;
 
 import javax.swing.JOptionPane;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
 
@@ -19,6 +16,7 @@ public class Main {
 
     public static int[] startState = new int[SIZE_OF_PUZZLE];
     public static int[] endState = new int[SIZE_OF_PUZZLE];
+    public static int[][] endStateMatrix = new int[NUMBER_OF_ROWS][NUMBER_OF_ROWS];
 
     public static void main(String[] args) {
         findLocationOfUsefulPieces();
@@ -26,14 +24,41 @@ public class Main {
         startState = receiveInput("start");
         endState = receiveInput("end");
 
+        endStateMatrix = convertToMatrix(endState);
+
         //determineIfStatesSolvable();
 
         Integer[] possibleSwaps = findPossibleSwaps(startState);
         HashMap<Integer,Integer> possibleMovementsWithH = calculateHValueOfPossibleMovements(possibleSwaps);
-        displayPossibleMovements(possibleSwaps);
-        // calculate h(distances of tiles from their pos) function of each movement
+        displayPossibleMovements(possibleMovementsWithH);
     }
 
+    /**
+     * Receives a state in array form and converts it to a matrix
+     * @param endState
+     * @return
+     */
+    private static int[][] convertToMatrix(int[] endState) {
+        int[][] matrix = new int[NUMBER_OF_ROWS][NUMBER_OF_ROWS];
+        int index = 0;
+        for(int i = 0;i<NUMBER_OF_ROWS;i++){
+            for(int j =0;j<NUMBER_OF_ROWS;j++){
+                matrix[i][j] = endState[index];
+                index++;
+            }
+        }
+
+        return matrix;
+    }
+
+    /**
+     * Returns key value pair of possible movement along with h value
+     * - loops through possible swaps
+     * - gets state if swap applied
+     * - get h value of state
+     * @param possibleSwaps
+     * @return
+     */
     private static HashMap<Integer, Integer> calculateHValueOfPossibleMovements(Integer[] possibleSwaps) {
         //TODO will have to update to apply to current state not just start
         HashMap<Integer,Integer> possibleMovementsWithHValue = new HashMap<>();
@@ -41,32 +66,57 @@ public class Main {
             if(possibleSwaps[i] != null){
                     int[] appliedState = applyPossibleSwapToStartState(possibleSwaps[i]);
                     int hValueForState = calculateHForStateAgainstEndState(appliedState);
-//                    possibleMovementsWithH.put() calculateHForstate()
+                    possibleMovementsWithHValue.put(possibleSwaps[i],hValueForState);
+            }
+            else{
+                possibleMovementsWithHValue.put(null,null);
             }
         }
         return possibleMovementsWithHValue;
     }
 
+    /**
+     * Returns the H value for the state by returning converting to matrix and getting square distance
+     * @param appliedState
+     * @return
+     */
     private static int calculateHForStateAgainstEndState(int[] appliedState) {
 
         int hValue = 0;
-        int tempHValue = 0;
-        boolean foundComparingValue = false;
+        int[][] convertedAppliedState = convertToMatrix(appliedState);
 
-        for(int i =0; i < SIZE_OF_PUZZLE;i++){
-            int comparingValue = appliedState[i];
-            for(int j =0;j< SIZE_OF_PUZZLE || !foundComparingValue;j++){
-                if(comparingValue == endState[j]){
-                    foundComparingValue = true;
-                }
-                tempHValue++;
-            }
-            hValue+= tempHValue;
-            foundComparingValue = false;
+        //for each tile in puzzle
+        for(int tile =0;tile<SIZE_OF_PUZZLE;tile++){
+            int indexOfTileEndState = getIndexOfTile(tile,endStateMatrix);
+            int indexOfTileAppliedState = getIndexOfTile(tile,convertedAppliedState);
+            hValue += Math.abs(indexOfTileAppliedState - indexOfTileEndState);
         }
+
         return hValue;
     }
 
+    /**
+     * Returns the sum of the index of the tile in the passed matrix eg (2,2) = 4
+     * @param tile
+     * @param stateMatrix
+     * @return
+     */
+    private static int getIndexOfTile(int tile, int[][] stateMatrix) {
+        for(int i = 0;i<NUMBER_OF_ROWS;i++){
+            for(int j = 0;j<NUMBER_OF_ROWS;j++){
+                if(stateMatrix[i][j] == tile){
+                    return(i+j);
+                }
+            }
+        }
+        return 0;
+    }
+
+    /**
+     * Returns the puzzle state if the passed swap index was applied
+     * @param possibleSwap
+     * @return
+     */
     private static int[] applyPossibleSwapToStartState(Integer possibleSwap) {
         int[] state = startState.clone();
         int temp = 0;
@@ -78,20 +128,24 @@ public class Main {
     }
 
     /**
-     * Loops through available movements and displays possible movements in JOptionPane
+     * Loops through available movements and displays possible movements in JOptionPane along with corresponding h value
      * @param possibleSwaps
      */
-    private static void displayPossibleMovements(Integer[] possibleSwaps) {
+    private static void displayPossibleMovements(HashMap<Integer,Integer> possibleSwaps) {
         String outputString = "";
-        for(int i=0;i<possibleSwaps.length;i++){
-            if(possibleSwaps[i] != null){
-                switch(i){
-                    case(0): outputString += formatPossibleMovementForDisplay(possibleSwaps[i],"South"); break;
-                    case(1): outputString += formatPossibleMovementForDisplay(possibleSwaps[i],"West"); break;
-                    case(2): outputString += formatPossibleMovementForDisplay(possibleSwaps[i],"North"); break;
-                    case(3): outputString += formatPossibleMovementForDisplay(possibleSwaps[i],"East"); break;
+        int direction = 0;
+        Iterator<Map.Entry<Integer, Integer>> iterator = possibleSwaps.entrySet().iterator();
+        while(iterator.hasNext()){
+            Map.Entry<Integer, Integer> next = iterator.next();
+            if(next.getKey() != null){
+                switch(direction){
+                    case(0): outputString += formatPossibleMovementForDisplay(next.getKey(),next.getValue(),"South"); break;
+                    case(1): outputString += formatPossibleMovementForDisplay(next.getKey(),next.getValue(),"West"); break;
+                    case(2): outputString += formatPossibleMovementForDisplay(next.getKey(),next.getValue(),"North"); break;
+                    case(3): outputString += formatPossibleMovementForDisplay(next.getKey(),next.getValue(),"East"); break;
                 }
             }
+            direction++;
         }
         JOptionPane.showMessageDialog(null, outputString, "Available Moves", JOptionPane.INFORMATION_MESSAGE);
     }
@@ -102,8 +156,8 @@ public class Main {
      * @param direction
      * @return
      */
-    private static String formatPossibleMovementForDisplay(Integer possibleSwap, String direction) {
-        return "Can move " + String.valueOf(startState[possibleSwap]) + " to the " + direction + "\n";
+    private static String formatPossibleMovementForDisplay(Integer possibleSwap,Integer hValue, String direction) {
+        return "Can move " + String.valueOf(startState[possibleSwap]) + " to the " + direction + " with a h Value of " + hValue + "\n";
     }
 
     /**
@@ -265,7 +319,6 @@ public class Main {
         if(uniqueNumbers.size() != SIZE_OF_PUZZLE){
             return false;
         }
-        System.out.print("valid");
         return true;
     }
 
